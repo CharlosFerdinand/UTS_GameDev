bda1d973cc81581b5a19f97fc3f48a07023cb160
randogue <121857862+randogue@users.noreply.github.com>
2025-11-23 23:52:47 +0700

    Added FOV option in pause menu's setting

--- Assets/DwRoomGeneration/UI/DwButtonHandler.cs
+++ Assets/DwRoomGeneration/UI/DwButtonHandler.cs
@@ -3,6 +3,12 @@ using UnityEngine.SceneManagement;
 
 public class DwButtonHandler : MonoBehaviour
 {
+    private GameObject canvas;
+
+    private void Start()
+    {
+        canvas = GameObject.Find("Canvas");
+    }
     public void continueBtn()
     {
         Time.timeScale = 1f; //continue the time
@@ -20,5 +26,21 @@ public class DwButtonHandler : MonoBehaviour
         Cursor.lockState = CursorLockMode.None;
         SceneManager.LoadScene("MainMenu");
         Time.timeScale = 1f;
+    }
+
+
+
+
+    //SETTING MENU ===================================================================
+    
+    public void settingButton()
+    { //open setting
+        GameObject settingMenu = canvas.transform.Find("PauseScreen").Find("SettingMenu").gameObject;
+        settingMenu.SetActive(true);
+    }
+
+    public void closeSettingButton()
+    { //close settin
+        canvas.transform.Find("PauseScreen").Find("SettingMenu").gameObject.SetActive(false);
     }
 }
---
+++ Assets/DwRoomGeneration/UI/SettingScript.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class SettingScript : MonoBehaviour
+{
+    public TMP_Text fovTitle;
+    public void fovSlider(Slider slider)
+    {
+        float fov = slider.value;
+        if (fovTitle != null)
+        {
+            fovTitle.text = "FOV(" + ((int)fov).ToString() + "):";
+        }
+        Camera.main.fieldOfView = fov;
+    }
+}
